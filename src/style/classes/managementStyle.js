02a3f0c365475f1ea21528cbe336bc4e8b7c7256
jikuza <striker.gab123@gmail.com>
2021-07-06 15:56:21 +0800

    PUL-2245

--- src/style/classes/managementStyle.js
+++ src/style/classes/managementStyle.js
@@ -809,6 +809,9 @@ export const getManagmentStyle=(windowSize,isRTL,theme) => ({
   link: {
     textDecoration: 'underline',
     margin: '0 5px'
+  },
+  duplicateSuccessMsg: {
+    padding: '0 25px!important'
   }
   
 })
