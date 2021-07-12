75d0ac864fbc88326ea17f4627018dc4bcd43928
Ido Shahar <idoshahar@gmail.com>
2021-07-12 15:03:05 +0300

    Edit cuplicated campaign popup marked as comment

--- src/screens/LandingPages/Management/LandingPagesManagment.js
+++ src/screens/LandingPages/Management/LandingPagesManagment.js
@@ -619,10 +619,10 @@ const LandingPagesesManagmentScreen=({classes}) => {
       const { payload={} } = result || {};
       await getData()
       setLoader(false);
-      setDialogType({
-        type: 'duplicateSuccessful',
-        data: payload
-      })
+      // setDialogType({
+      //   type: 'duplicateSuccessful',
+      //   data: payload
+      // })
     }
   })
 
@@ -669,8 +669,8 @@ const LandingPagesesManagmentScreen=({classes}) => {
     const dialogContent={
       restore: getRestorDialog(data),
       delete: getDeleteDialog(data),
-      duplicate: getDuplicateDialog(data),
-      duplicateSuccessful: getDuplicateSuccessfulDialog(data)
+      duplicate: getDuplicateDialog(data) //,
+      // duplicateSuccessful: getDuplicateSuccessfulDialog(data)
     }
 
     const currentDialog=dialogContent[type]||{}
