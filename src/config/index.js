56e715377c1edb9350172a64e6196296675ed7a1
idoshahar <ido@pulseem.com>
2021-07-13 14:35:54 +0300

    semicolon

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 // }=process.env;
 
 const apiURL = process.env.REACT_APP_API_URL;
-const isProdMode=process.env.REACT_APP_MODE==='PROD'
+const isProdMode=process.env.REACT_APP_MODE==='PROD';
 const actionURL = isProdMode? `https://${window.location.hostname}/Pulseem/` : process.env.REACT_APP_ACTION_URL;
 
 export {
