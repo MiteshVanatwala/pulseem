b31c9f139442f722b615dc9cefb7eb90fa5feb76
idoshahar <ido@pulseem.com>
2021-09-05 17:25:04 +0300

    PUL-2563

--- src/style/classes/dashboardStyles.js
+++ src/style/classes/dashboardStyles.js
@@ -1,7 +1,143 @@
+const dashboardDirection = {
+    xs: 'column-reverse',
+    sm: '',
+    md: '',
+    lg: '',
+    xl: ''
+}
+
+const shortcutEditLeft = {
+    xs: '8%',
+    sm: '18px',
+    md: '15px',
+    lg: '22px',
+    xl: '28px'
+}
+
+const shortcutPaperHeight = {
+    xs: '',
+    sm: '100vh',
+    md: '100vh',
+    lg: '95vh',
+    xl: '95vh'
+}
+
 export const getDashboardStyle = (windowSize, isRTL, theme) => ({
+    mobileReportHead: {
+        fontWeight: 'bold',
+        marginTop: 15,
+        marginInlineStart: 10
+    },
+    tabelCellPadding: {
+        paddingBlock: 15
+    },
+    dashboard: {
+        background: '#F2F2F2',
+        padding: 0,
+        maxHeight: 'unset'
+    },
+    dashboardContainer: {
+        flexDirection: dashboardDirection[windowSize]
+
+    },
+    chartLabelGreen: {
+        position: 'absolute',
+        top: 'calc(50% - 28px)',
+        width: '100%',
+        fontSize: 35,
+        fontWeight: '500',
+        textTransform: 'uppercase',
+        color: '#65d638'
+    },
+    shortcutTitleSection: {
+        width: '85%',
+        marginTop: '2.1rem',
+        marginBottom: 40
+    },
+    shortcutTitle: {
+        fontSize: windowSize === 'xs' ? 23 : 25,
+        fontWeight: 'bold'
+    },
+    shortcutSubtitle: {
+        fontSize: windowSize === 'xs' ? 16 : 18,
+    },
+    shortcutBox: {
+        position: windowSize === 'xs' ? '' : 'sticky',
+        top: 0,
+        height: shortcutPaperHeight[windowSize],
+        [theme.breakpoints.down('xs')]: {
+            margin: '10px 10px -10px 10px'
+        },
+        boxShadow: isRTL ? '4px 0px 5px 0px rgba(0,0,0, 0.2)' : '-4px 0px 5px 0px rgba(0,0,0, 0.2) '
+    },
+    shortcutPaper: {
+        [theme.breakpoints.down('xs')]: {
+            borderRadius: 10,
+        },
+        height: shortcutPaperHeight[windowSize],
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        overflow: 'auto',
+        '&::-webkit-scrollbar': {
+            width: '0px'
+        },
+        '&::-webkit-scrollbar-thumb': {
+            'backgroundColor': 'darkgrey',
+            borderRadius: '5px'
+        }
+    },
+    shortcutBtnBox: {
+        position: 'relative',
+        width: '100%',
+        textAlign: 'center'
+    },
+    shortcutButton: {
+        height: '92px',
+        width: '85%',
+        background: '#0371AD',
+        borderRadius: '20px',
+        marginBottom: windowSize === 'xs' ? 30 : 45,
+        fontSize: '18px',
+        position: 'relative',
+        textTransform: 'capitalize',
+        padding: '6px 30px'
+
+    },
+    shortcutDottedButton: {
+        borderRadius: '20px',
+        border: '1px dashed #0371AD',
+        fontFamily: 'pulseemicons',
+        height: '92px',
+        fontSize: '30px',
+        marginBottom: 45,
+        width: '85%',
+    },
+    shortcutList: {
+        maxWidth: 350,
+        background: 'white',
+        borderRadius: 10,
+        margin: 10
+    },
+    shortcutEditIcon: {
+        position: 'absolute',
+        left: isRTL ? 'auto' : shortcutEditLeft[windowSize],
+        right: isRTL ? shortcutEditLeft[windowSize] : 'auto',
+        bottom: windowSize === 'xs' ? 40 : 50,
+        fontFamily: 'pulseemicons',
+        fontSize: 18,
+        color: 'white'
+    },
+    shortcutLabel: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        marginTop: -5
+    },
     deleteShortcut: {
         position: 'absolute',
-        right: isRTL ? 40 : 'auto',
+        right: isRTL ? 'auto' : 40,
+        left: isRTL ? 40 : 'auto',
         top: 5,
         zIndex: 100,
         color: '#fff',
