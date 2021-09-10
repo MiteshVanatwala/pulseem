0284eb41514ca6d6b6b27a5d6b2fecda8fa3f449
idoshahar <ido@pulseem.com>
2021-09-10 15:09:35 +0300

    Remove pulseem/ from the link

--- src/components/Reports/LatestReports.js
+++ src/components/Reports/LatestReports.js
@@ -168,7 +168,7 @@ const LatestReports = ({ classes, windowSize, t, isRTL }) => {
           <Grid item lg={4} className={tabType !== "newsletter" ? classes.flexSpaceBetweenVertical : null}>
             {
               innerData.map((c, index) => {
-                const campaignLink = `${actionURL}Pulseem/CampaignStatistics.aspx?CampaignID=${c.CampaignID}` 
+                const campaignLink = `${actionURL}/CampaignStatistics.aspx?CampaignID=${c.CampaignID}` 
                 return (
                   <Grid container className={clsx(tabType === "newsletter" ? classes.mb25 : null, tabType === "newsletter" ? classes.mt25 : null)} key={`${c.CampaignName}_${index}`}>
                     <Grid item lg={12}>
