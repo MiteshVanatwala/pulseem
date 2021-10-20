0a8e91bf650fc0313178f0ab9599598454c264d1
Ido Shahar <idoshahar@gmail.com>
2021-10-20 11:32:03 +0300

    Fix campaign/lp url

--- src/screens/Sms/Editor/SmsCreator.js
+++ src/screens/Sms/Editor/SmsCreator.js
@@ -1276,11 +1276,11 @@ const SmsCreator = ({ classes, ...props }) => {
   const handleAddLink = async (id, linkType) => {
     let text = "";
     if (linkType === 'campaign') {
-      text = previousLandingData[id].PageHref;
+      text = previousCampaignData[id].PageHref;
       setdialogClickCampaign(false);
     }
     else if (linkType === 'lp') {
-      text = previousCampaignData[id].EncryptURL
+      text = previousLandingData[id].EncryptURL
       setdialogClickLanding(false);
     }
     seteditmenuClick(false);
