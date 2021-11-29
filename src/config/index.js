b85588d8c4738a61180e3e8985db14bd3c4136b3
Ido Shahar <idoshahar@gmail.com>
2021-11-29 11:52:30 +0200

    Add siteTrackingURL in env file so it will be configurable

--- src/config/index.js
+++ src/config/index.js
@@ -4,9 +4,11 @@ dotenv.config();
 const apiURL = process.env.REACT_APP_API_URL; 
 const isProdMode=process.env.REACT_APP_MODE==='PROD';
 const actionURL = isProdMode? `https://${window.location.hostname}/Pulseem/` : process.env.REACT_APP_ACTION_URL;
+const siteTrackingURL = process.env.SITE_TRACKING_API_URL
 
 export {
   apiURL,
   actionURL,
-  isProdMode
+  isProdMode,
+  siteTrackingURL
 }
