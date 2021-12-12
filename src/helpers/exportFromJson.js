4effbeab2aca72c78ac5a88f6eca1ee0a883ba73
idoshahar <ido@pulseem.com>
2021-12-12 12:19:43 +0200

    Remove comments

--- src/helpers/exportFromJson.js
+++ src/helpers/exportFromJson.js
@@ -1,21 +1,6 @@
 import exportFromJSON from 'export-from-json'
 
 export const exportFile = ({ data = [], fileName = 'PulseemReport', exportType = 'xls', fields = null }) => {
-  // let table = '<table>';
-  // let tr;
-  // for (var i = 0; i < data.length; i++) {
-  //   tr = '<tr>';
-  //   let tds = '';
-  //   Object.keys(data[i]).forEach((k, index) => {
-  //     tds += "<td>" + data[i][k] + "</td>";
-  //   });
-  //   tr += tds;
-  //   tr += '</tr>';
-  //   table += tr;
-  // }
-  // table += '</table>'
-
-  // table = table.replace('##tableData##', tr);
   exportFromJSON({
     data,
     fileName,
