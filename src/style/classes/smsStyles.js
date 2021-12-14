824aacc0b7093f0673de192e8e18e5b007912b62
idoshahar <ido@pulseem.com>
2021-12-14 12:26:48 +0200

    Fix edit client popup

--- src/style/classes/smsStyles.js
+++ src/style/classes/smsStyles.js
@@ -38,6 +38,14 @@ const flexDirection = {
     xl: 'row',
 }
 
+const clientIframeSize = {
+    xs: '100%',
+    sm: '100%',
+    md: 680,
+    lg: 680,
+    xl: 680
+}
+
 export const getSmsStyle = (windowSize, isRTL, theme) => ({
     sectionTitle: {
         marginTop: 45,
@@ -988,5 +996,10 @@ export const getSmsStyle = (windowSize, isRTL, theme) => ({
         padding: "10px",
         width: "160px",
         maxWidth: "280px",
+    },
+    editClientIframe: {
+        minWidth: clientIframeSize[windowSize],
+        minHeight: 610,
+        border: 'none'
     }
 });
