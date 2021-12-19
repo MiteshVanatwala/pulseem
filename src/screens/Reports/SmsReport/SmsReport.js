6d829a973aa959d1b7895547810f2d714fcf9bc2
Ido Shahar <ido@pulseem.com>
2021-12-19 11:03:28 +0200

    Fix from source

--- src/screens/Reports/SmsReport/SmsReport.js
+++ src/screens/Reports/SmsReport/SmsReport.js
@@ -80,7 +80,8 @@ const SmsReport = ({ classes }) => {
     },
     Replies: {
       title: t('common.Total'),
-      href: `/Pulseem/SmsReplies.aspx?ReplyCountSMSCampaignID=${id}&Culture=${isRTL ? 'he-IL' : 'en-US'}`
+      // href: `/Pulseem/SmsReplies.aspx?ReplyCountSMSCampaignID=${id}&Culture=${isRTL ? 'he-IL' : 'en-US'}`
+      href: `/Pulseem/ResponsesReport.aspx?SmsCampaignID=${id}&Culture=${isRTL ? 'he-IL' : 'en-US'}`
     },
     DLR: {
       title: t('common.DLR'),
