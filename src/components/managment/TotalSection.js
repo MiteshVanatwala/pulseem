cb785539934c9047e38e18af02e40744d5cf6045
idoshahar <ido@pulseem.com>
2022-01-20 18:10:53 +0200

    PUL-3022

--- src/components/managment/TotalSection.js
+++ src/components/managment/TotalSection.js
@@ -2,18 +2,25 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import clsx from 'clsx';
 import { Box, Grid, Typography } from '@material-ui/core';
+import { useSelector } from 'react-redux';
 
 const TotalSection = ({ classes, TotalObject }) => {
     const { t } = useTranslation();
+    const { windowSize } = useSelector(state => state.core);
 
-    if (TotalObject) {
+    if (typeof TotalObject === 'object') {
         return <Box className={clsx(classes.paddingSides25, classes.mb10, classes.reportPaperBgGray, classes.alignCenter)} style={{ marginBottom: 50 }}>
             <Grid item container className={clsx(classes.justifyEvenly)} style={{ width: '100%' }}>
                 {Object.keys(TotalObject).map((to) => {
                     if (typeof TotalObject[to] === 'object') {
                         return false;
                     }
-                    return <Grid item className={clsx(classes.txtCenter, classes.pt14)}>
+                    if (windowSize === 'xs') {
+                        if (to === 'TotalCredits' || to === 'TotalRecords') {
+                            return false;
+                        }
+                    }
+                    return <Grid item className={clsx(classes.txtCenter, classes.pt14)} style={{ maxWidth: windowSize === 'xs' ? 100 : null }}>
                         <Typography className={clsx(classes.bold, classes.colorBlue)}>
                             {t(`report.${to}`)}
                         </Typography>
