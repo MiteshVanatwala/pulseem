120701f11a2f828545277f47a8a511237f9ad1e6
Ido Shahar <idoshahar@gmail.com>
2022-01-24 12:25:25 +0200

    revert

--- src/App.js
+++ src/App.js
@@ -431,6 +431,13 @@ const App = ({ screenSize }) => {
       jtoken: updateToken
     }
 
+    const insertScript = () => {
+      const script = document.createElement("script");
+      script.src = `${siteTrackingScriptUrl}`; //?v=` + Math.floor(Date.now() / 1000);
+      script.async = false;
+      document.head.appendChild(script);
+    }
+
     // window.addEventListener('resize',setWindowWidth)
     cookieListener(({ name }) => {
       const cookieFunction = cookieFunctionObj[name] || null
@@ -439,22 +446,15 @@ const App = ({ screenSize }) => {
     })
     updateToken()
     initFeatures()
+    //Remove this implementation before uploading
+    insertScript()
   }, [dispatch])
 
 
-  const insertScript = () => {
-    const script = document.createElement("script");
-    script.src = `${siteTrackingScriptUrl}`; //?v=` + Math.floor(Date.now() / 1000);
-    script.async = false;
-    document.head.appendChild(script);
-  }
-  
   const classes = useClasses(windowSize, isRTL)()
   const theme = getTheme(language)
   const history = useHistory()
   document.body.classList.add(classes.sidebar);
-  //Remove this implementation before uploading
-  insertScript()
 
   if (isRTL) document.body.classList.add('rtl');
   else document.body.classList.remove('rtl');
