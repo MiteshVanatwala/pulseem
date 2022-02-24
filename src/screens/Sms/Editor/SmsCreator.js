39a0c82a35fcabb71cb13fc4c16b079e4791e286
idoshahar <ido@pulseem.com>
2022-02-24 11:43:04 +0200

    PUL-3076

--- src/screens/Sms/Editor/SmsCreator.js
+++ src/screens/Sms/Editor/SmsCreator.js
@@ -443,7 +443,7 @@ const SmsCreator = ({ classes, ...props }) => {
     if (t && t.length > 0) {
       const res = t.replace('\r\n', ' ');
       // eslint-disable-next-line
-      const regex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_##]*)?\??(?:[{}\-\+=&;,%@\.\w_]*)##?(?:[\.\!\/\\\w+##]*))?)/g;
+      const regex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_#]*)?\??(?:[{}\-\+=&;,%@\.\w_]*)#?(?:[\.\!\/\\\w+#]*))?)/g;
       const links = res.match(regex);
 
       if (links && links.length > 0) {
