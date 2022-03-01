c78b3c4a2310077074b89508f6c89a04a4ae361b
Ido Shahar <idoshahar@gmail.com>
2022-03-01 10:01:53 +0200

    Play around

--- src/screens/Newsletter/Editor/CampaignEditor.js
+++ src/screens/Newsletter/Editor/CampaignEditor.js
@@ -4,29 +4,59 @@ import EmailEditor from 'react-email-editor'
 import DefaultScreen from '../../DefaultScreen'
 
 const CampaignEditor = ({ classes }) => {
-    const editor = useRef(null);
+    const editorRef = useRef(null);
 
     const exportHtml = () => {
-        editor.current.exportHtml(data => {
+        editorRef.current.exportHtml(data => {
             const { design, html } = data
             console.log('exportHtml', html)
             console.log('design', design)
         })
     }
     const saveDesign = () => {
-        editor.current.saveDesign(design => {
+        editorRef.current.saveDesign(design => {
             console.log('saveDesign', design)
         })
     }
 
-    const onLoad = () => {
-        console.log('onReady');
-        editor.current.tools = {
-            html: {
-                enabled: false
+    const appearance = {
+        panels: {
+            tools: {
+                dock: 'left'
             }
         }
+    }
 
+    const tools = {
+        html: {
+            enabled: false
+        },
+        ido: {
+            name: 'my_tool',
+            label: 'My Tool',
+            icon: 'fa-smile',
+            supportedDisplayModes: ['web', 'email'],
+            options: {},
+            values: {},
+            renderer: {
+                Viewer: (values) => {
+                    return "<div>I am a custom tool.</div>"
+                },
+                exporters: {
+                    web: function (values) {
+                        return "<div>I am a custom tool.</div>"
+                    },
+                    email: function (values) {
+                        return "<div>I am a custom tool.</div>"
+                    }
+                },
+                head: {
+                    css: function (values) { },
+                    js: function (values) { }
+                }
+
+            }
+        }
     };
 
     return (
@@ -34,9 +64,14 @@ const CampaignEditor = ({ classes }) => {
             currentPage='campaignEditor'
             classes={classes}
         >
-            <EmailEditor ref={editor} minHeight="calc(100vh - 100px)" onLoad={onLoad} />
             <Button onClick={exportHtml}>Export HTML</Button>
             <Button onClick={saveDesign}>Save Design</Button>
+            <EmailEditor
+                ref={editorRef}
+                minHeight="calc(100vh - 100px)"
+                tools={tools}
+                appearance={appearance}
+            />
         </DefaultScreen>
     )
 }
