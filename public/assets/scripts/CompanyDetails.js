93c55a5f5c0ec1741612b00ad7732aa8cd667ef6
idoshahar <ido@pulseem.com>
2022-04-21 12:45:26 +0300

    Small fix

--- public/assets/scripts/CompanyDetails.js
+++ public/assets/scripts/CompanyDetails.js
@@ -10,40 +10,40 @@ unlayer.registerTool({
   renderer: {
     Viewer: unlayer.createViewer({
       render(values) {
-        return `<div style="font-weight: bold; text-align: center" contenteditable="true">
+        return `<div style="font-weight: bold; text-align: center">
         <span style="display: ${values.data.phone !== '' ? 'inline-block' : 'none'}">${values.data.phoneTitle}: ${values.data.phone} | </span>
         <span style="display: ${values.data.fax !== '' ? 'inline-block' : 'none'}">${values.data.faxTitle}: ${values.data.fax} | </span>
-        <span style="display: ${values.data.email !== '' ? 'inline-block' : 'none'}">${values.data.emailTitle}: <a href="mailto:${values.data.email}"}>${values.data.email}</a></span>
+        <span style="display: ${values.data.email !== '' ? 'inline-block' : 'none'}">${values.data.emailTitle}: <a href="mailto:${values.data.email}">${values.data.email}</a></span>
         <br />
-        <span style="display: ${values.data.company !== '' ? 'inline-block' : 'none'}">${values.data.company}</span>, 
-        <span style="display: ${values.data.address !== '' ? 'inline-block' : 'none'}">${values.data.address}</span>, 
-        <span style="display: ${values.data.city !== '' ? 'inline-block' : 'none'}">${values.data.city}</span>, 
+        <span style="display: ${values.data.company !== '' ? 'inline-block' : 'none'}">${values.data.company}</span>,
+        <span style="display: ${values.data.address !== '' ? 'inline-block' : 'none'}">${values.data.address}</span>,
+        <span style="display: ${values.data.city !== '' ? 'inline-block' : 'none'}">${values.data.city}</span>,
         <span style="display: ${values.data.country !== '' ? 'inline-block' : 'none'}">${values.data.country}</span>
     </div>`
       }
     }), // our React Viewer
     exporters: {
       web: function (values) {
-        return `<div style="font-weight: bold; text-align: center" contenteditable="true">
+        return `<div style="font-weight: bold; text-align: center">
         <span style="display: ${values.data.phone !== '' ? 'inline-block' : 'none'}">${values.data.phoneTitle}: ${values.data.phone} | </span>
         <span style="display: ${values.data.fax !== '' ? 'inline-block' : 'none'}">${values.data.faxTitle}: ${values.data.fax} | </span>
-        <span style="display: ${values.data.email !== '' ? 'inline-block' : 'none'}">${values.data.emailTitle}: <a href="mailto:${values.data.email}"}>${values.data.email}</a></span>
+        <span style="display: ${values.data.email !== '' ? 'inline-block' : 'none'}">${values.data.emailTitle}: <a href="mailto:${values.data.email}">${values.data.email}</a></span>
         <br />
-        <span style="display: ${values.data.company !== '' ? 'inline-block' : 'none'}">${values.data.company}</span>, 
-        <span style="display: ${values.data.address !== '' ? 'inline-block' : 'none'}">${values.data.address}</span>, 
-        <span style="display: ${values.data.city !== '' ? 'inline-block' : 'none'}">${values.data.city}</span>, 
+        <span style="display: ${values.data.company !== '' ? 'inline-block' : 'none'}">${values.data.company}</span>,
+        <span style="display: ${values.data.address !== '' ? 'inline-block' : 'none'}">${values.data.address}</span>,
+        <span style="display: ${values.data.city !== '' ? 'inline-block' : 'none'}">${values.data.city}</span>,
         <span style="display: ${values.data.country !== '' ? 'inline-block' : 'none'}">${values.data.country}</span>
     </div>`
       },
       email: function (values) {
-        return `<div style="font-weight: bold; text-align: center" contenteditable="true">
+        return `<div style="font-weight: bold; text-align: center">
         <span style="display: ${values.data.phone !== '' ? 'inline-block' : 'none'}">${values.data.phoneTitle}: ${values.data.phone} | </span>
         <span style="display: ${values.data.fax !== '' ? 'inline-block' : 'none'}">${values.data.faxTitle}: ${values.data.fax} | </span>
-        <span style="display: ${values.data.email !== '' ? 'inline-block' : 'none'}">${values.data.emailTitle}: <a href="mailto:${values.data.email}"}>${values.data.email}</a></span>
+        <span style="display: ${values.data.email !== '' ? 'inline-block' : 'none'}">${values.data.emailTitle}: <a href="mailto:${values.data.email}">${values.data.email}</a></span>
         <br />
-        <span style="display: ${values.data.company !== '' ? 'inline-block' : 'none'}">${values.data.company}</span>, 
-        <span style="display: ${values.data.address !== '' ? 'inline-block' : 'none'}">${values.data.address}</span>, 
-        <span style="display: ${values.data.city !== '' ? 'inline-block' : 'none'}">${values.data.city}</span>, 
+        <span style="display: ${values.data.company !== '' ? 'inline-block' : 'none'}">${values.data.company}</span>,
+        <span style="display: ${values.data.address !== '' ? 'inline-block' : 'none'}">${values.data.address}</span>,
+        <span style="display: ${values.data.city !== '' ? 'inline-block' : 'none'}">${values.data.city}</span>,
         <span style="display: ${values.data.country !== '' ? 'inline-block' : 'none'}">${values.data.country}</span>
     </div>`
       }
