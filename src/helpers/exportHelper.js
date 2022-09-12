0fcb562c7739521b7d9488addfd45dffea71e505
Ido Shahar <idoshahar@gmail.com>
2022-09-12 16:47:34 +0300

    PR-695

--- src/helpers/exportHelper.js
+++ src/helpers/exportHelper.js
@@ -2,54 +2,54 @@ import i18n from 'i18next';
 import moment from 'moment';
 import 'moment/locale/he';
 
-export const preferredOrder = (obj, order) => {
-    const arr = [];
-    for (var i = 0; i < obj.length; i++) {
-        let newObject = {};
-        // eslint-disable-next-line no-loop-func
-        order.forEach((o) => {
-            newObject[o] = obj[i][o];
-        });
-        arr.push(newObject);
-    }
-    return arr;
-}
+// export const preferredOrder = (obj, order) => {
+//     const arr = [];
+//     for (var i = 0; i < obj.length; i++) {
+//         let newObject = {};
+//         // eslint-disable-next-line no-loop-func
+//         order.forEach((o) => {
+//             newObject[o] = obj[i][o];
+//         });
+//         arr.push(newObject);
+//     }
+//     return arr;
+// }
 
-export const switchStatusDescription = (obj, statuses) => {
-    obj.forEach((o) => {
-        if (o.STATUS) {
-            let status = statuses.find((s) => { return s.id === o.STATUS });
-            o.StatusDescription = i18n.t(status ? status.value : null);
-        }
-        else if (o.Status) {
-            let status = statuses.find((s) => { return s.id === o.Status });
-            o.StatusDescription = i18n.t(status ? status.value : null);
-        }
-    });
-    return obj;
-}
+// export const switchStatusDescription = (obj, statuses) => {
+//     obj.forEach((o) => {
+//         if (o.STATUS) {
+//             let status = statuses.find((s) => { return s.id === o.STATUS });
+//             o.StatusDescription = i18n.t(status ? status.value : null);
+//         }
+//         else if (o.Status) {
+//             let status = statuses.find((s) => { return s.id === o.Status });
+//             o.StatusDescription = i18n.t(status ? status.value : null);
+//         }
+//     });
+//     return obj;
+// }
 
-export const replaceNull = (obj, property, val = '') => {
-    obj.forEach((o) => {
-        if (o[property] === null || o[property] === '') {
-            o[property] = val;
-        }
-    });
-    return obj;
-}
+// export const replaceNull = (obj, property, val = '') => {
+//     obj.forEach((o) => {
+//         if (o[property] === null || o[property] === '') {
+//             o[property] = val;
+//         }
+//     });
+//     return obj;
+// }
 
-export const statusNumberToString = (t, obj, statuses) => {
-    obj.forEach((o) => {
-        if (o.Status) {
-            let status = statuses.find((s) => { return s.id === o.Status });
-            o.StatusName = t(status ? status.value : null);
-        }
-        if (o.Attachments && (o.Attachments === 'No_Attachments' || o.Attachments === '')) {
-            o.Attachments = t('emailStatus.noAttachments');
-        }
-    });
-    return obj;
-}
+// export const statusNumberToString = (t, obj, statuses) => {
+//     obj.forEach((o) => {
+//         if (o.Status) {
+//             let status = statuses.find((s) => { return s.id === o.Status });
+//             o.StatusName = t(status ? status.value : null);
+//         }
+//         if (o.Attachments && (o.Attachments === 'No_Attachments' || o.Attachments === '')) {
+//             o.Attachments = t('emailStatus.noAttachments');
+//         }
+//     });
+//     return obj;
+// }
 
 export const booleanToNumber = (obj, column, isBoolean = false, t) => {
     obj.forEach((o) => {
