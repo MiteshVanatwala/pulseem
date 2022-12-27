e13df293f4bc9bf162b5f4285fd3f3daa73b9efb
sachinkumar <sachinkumar@capitalnumbers.com>
2022-12-27 12:15:42 +0530

    made changes according rotem's comment

--- src/screens/Whatsapp/Editor/WhatsappCreator.tsx
+++ src/screens/Whatsapp/Editor/WhatsappCreator.tsx
@@ -363,6 +363,21 @@ const WhatsappCreator = ({ classes }: WhatsappCreatorProps & ClassesType) => {
 		updatedTemplateData.templateText = textData?.body;
 	};
 
+	const setUpdatedTemplateData = (templateData: savedTemplateDataProps) => {
+		if ('quick-reply' in templateData?.types) {
+			saveQuickreplyTemplate(templateData);
+		}
+		if ('call-to-action' in templateData?.types) {
+			saveCallToActionTemplate(templateData);
+		} else if ('card' in templateData?.types) {
+			saveCardTemplate(templateData);
+		} else if ('media' in templateData?.types) {
+			saveMediaTemplate(templateData);
+		} else if ('text' in templateData?.types) {
+			saveTextTemplate(templateData);
+		}
+	};
+
 	const onSavedTemplateChange = (TemplateId: string) => {
 		setSavedTemplate(TemplateId);
 		const savedTemplateData: savedTemplateListProps | undefined =
@@ -370,18 +385,7 @@ const WhatsappCreator = ({ classes }: WhatsappCreatorProps & ClassesType) => {
 		const templateData: savedTemplateDataProps | undefined =
 			savedTemplateData?.Data;
 		if (templateData) {
-			if ('quick-reply' in templateData?.types) {
-				saveQuickreplyTemplate(templateData);
-			}
-			if ('call-to-action' in templateData?.types) {
-				saveCallToActionTemplate(templateData);
-			} else if ('card' in templateData?.types) {
-				saveCardTemplate(templateData);
-			} else if ('media' in templateData?.types) {
-				saveMediaTemplate(templateData);
-			} else if ('text' in templateData?.types) {
-				saveTextTemplate(templateData);
-			}
+			setUpdatedTemplateData(templateData);
 		}
 		setFileData(updatedFileData);
 		// setTemplateName(savedTemplateData?.TemplateName || '');
