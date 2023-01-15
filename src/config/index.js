ebf1752e1ab38d996691e9469cc5edd8e02e3b89
ido <ido@pulseem.com>
2023-01-15 16:44:34 +0200

    remove port 9999

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ dotenv.config();
 const apiURL = process.env.REACT_APP_API_URL;
 const isProdMode = process.env.REACT_APP_MODE === "PROD";
 const actionURL = isProdMode
-  ? `https://${window.location.hostname}:9999/Pulseem/`
+  ? `https://${window.location.hostname}/Pulseem/`
   : process.env.REACT_APP_ACTION_URL;
 const siteTrackingURL = process.env.REACT_APP_TRACKING_URL;
 const siteTrackingScriptUrl = process.env.REACT_APP_TRACKING_SCRIPT;
