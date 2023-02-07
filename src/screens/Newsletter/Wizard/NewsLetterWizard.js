730bf8029f14aba810292cb0418b61f48fee9448
ido <ido@pulseem.com>
2023-02-07 06:34:11 +0200

    small fix

--- src/screens/Newsletter/Wizard/NewsLetterWizard.js
+++ src/screens/Newsletter/Wizard/NewsLetterWizard.js
@@ -876,7 +876,7 @@ const NewsLetterWizard = ({ classes }) => {
             }
             else {
                 wizardButtons.push(<><Button
-                    onClick={() => showCautionOldEditor ? setDialogType({ type: "cautionOldEditor" }) : handleSubmit(true, false, false)}
+                    onClick={() => showCautionOldEditor ? setDialogType({ type: "cautionNewEditor" }) : handleSubmit(true, false, false)}
                     variant='contained'
                     size='medium'
                     className={clsx(
@@ -888,7 +888,7 @@ const NewsLetterWizard = ({ classes }) => {
                     color="primary"
                 >{t('common.saveAndContinue')}</Button>
                     {(id === null || id === undefined) && <Button
-                        onClick={() => showCautionNewEditor ? setDialogType({ type: "cautionNewEditor" }) : handleSubmit(true, false, true)}
+                        onClick={() => showCautionNewEditor ? setDialogType({ type: "cautionOldEditor" }) : handleSubmit(true, false, true)}
                         variant='contained'
                         size='medium'
                         className={clsx(
@@ -935,7 +935,7 @@ const NewsLetterWizard = ({ classes }) => {
                             {t('campaigns.newsLetterMgmt.newsLetterWizard.continueEditingInOldEditor')}
                         </Typography>
                         <Typography className={classes.f20}>
-                            {t('campaigns.newsLetterMgmt.newsLetterWizard.recreateInOldEditor')}
+                            {t('campaigns.newsLetterMgmt.newsLetterWizard.continueEditingInNewEditor')}
                         </Typography>
 
                         <Box className={classes.mt15}>
@@ -988,7 +988,7 @@ const NewsLetterWizard = ({ classes }) => {
                             {/* Pay attention! */}
                         </Typography>
                         <Typography className={classes.f20}>
-                            {t('campaigns.newsLetterMgmt.newsLetterWizard.continueEditingInNewEditor')}
+                            {t('campaigns.newsLetterMgmt.newsLetterWizard.recreateInOldEditor')}
                         </Typography>
                         <Typography className={classes.f20}>
                             {t('campaigns.newsLetterMgmt.newsLetterWizard.recreateInNewEditor')}
