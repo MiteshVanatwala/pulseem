ea8cc5bdd9683d814c7134fda48f97242ff71ecf
Ido Shahar <idoshahar@gmail.com>
2023-04-20 16:32:18 +0300

    PR-1000

--- src/components/NotificationBell/NotificationBell.js
+++ src/components/NotificationBell/NotificationBell.js
@@ -1,16 +1,16 @@
 import clsx from 'clsx';
-import { Box, MenuItem, MenuList, Popper, Badge } from '@material-ui/core'
+import { Box, MenuItem, MenuList, Popper, Badge, Button } from '@material-ui/core'
 import { useSelector, useDispatch } from 'react-redux';
 import NotificationIcon from '../../assets/images/notification.svg';
 import { useRef, useState } from 'react';
 import { useTranslation } from "react-i18next";
 import { markNotificationsAsRead } from '../../redux/reducers/notificationUpdateSlice';
 
-const NotificationBell = ({classes}) => {
-  const [ displayNotifications, toggleDisplayNotifications ] = useState(false);
+const NotificationBell = ({ classes }) => {
+  const [displayNotifications, toggleDisplayNotifications] = useState(false);
   const notificationIconRef = useRef(null)
   const { t } = useTranslation();
-  const { notificationUpdateList  } = useSelector(state => state.notificationUpdate)
+  const { notifyCenterList, unreadMessages } = useSelector(state => state.notificationUpdate)
   const dispatch = useDispatch();
 
   const handleClose = () => {
@@ -18,15 +18,35 @@ const NotificationBell = ({classes}) => {
   }
 
   const notificationItem = () => {
+    const notifyTemplate = (option) => {
+      switch (option.NotifyCenterTypeID) {
+        case 0: { // File
+          return <>your file is ready to donwload
+            <>
+              <Button onClick={() => { window.open(`/Pulseem/DownloadFile.aspx?fileFormat=CSV&fileId=${option.SourceID}`) }}>Download here</Button>
+            </>
+          </>
+        }
+        case 1: { // Unsubscribe
+          return <>Recipients were successfully removed from <b>"{option.TargetName}"</b></>
+        }
+        case 2: { // UploadRecipients
+          return <>Recipients were uploaded successfully to <b>"{option.TargetName}"</b></>
+        }
+        default: {
+          break;
+        }
+      }
+    }
     return (
       <MenuList>
         {
-          notificationUpdateList && notificationUpdateList.map((option, index) => (
+          notifyCenterList && notifyCenterList?.map((option, index) => (
             <MenuItem
-              key={option.ID}
+              key={option?.ID}
               className={clsx(classes.f12, classes.notificationItem, classes.paddingSides15)}
             >
-              {option.ActionName}
+              {notifyTemplate(option)}
             </MenuItem>
           ))
         }
@@ -40,10 +60,10 @@ const NotificationBell = ({classes}) => {
       onMouseLeave={handleClose}
       className={clsx(classes.appBarItemContainer, classes.paddingSides15)}>
       <Badge
-        badgeContent={notificationUpdateList.length}
+        badgeContent={unreadMessages}
         color="red"
         className={clsx(classes.bell)}
-        invisible={!notificationUpdateList.length}
+        invisible={unreadMessages === 0}
         max={99}
       >
         <img
@@ -58,7 +78,7 @@ const NotificationBell = ({classes}) => {
         />
       </Badge>
       <Popper open={displayNotifications} anchorEl={notificationIconRef.current} role={undefined} transition placement={'bottom-end'} disablePortal>
-        <div className={clsx(classes.notificationUpdateContainer, classes.p15, classes. pt10)}>
+        <div className={clsx(classes.notificationUpdateContainer, classes.p15, classes.pt10)}>
           <div className={clsx(classes.bold)}>
             {t('notifications.notifications')}
           </div>
--- src/redux/reducers/notificationUpdateSlice.js
+++ src/redux/reducers/notificationUpdateSlice.js
@@ -2,11 +2,11 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { instence } from '../../helpers/api'
 
 export const getNotificationUpdates = createAsyncThunk(
-  'NotificationCenter/GetUpdates', 
+  'NotifyCenter/GetUpdates',
   async (_, thunkAPI) => {
     try {
-      const response = await instence.get(`NotificationCenter/GetUpdates`);
-      return response.data.Data;
+      const response = await instence.get(`NotifyCenter/GetUpdates`);
+      return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue({ error: error.message });
     }
@@ -14,10 +14,10 @@ export const getNotificationUpdates = createAsyncThunk(
 );
 
 export const markNotificationsAsRead = createAsyncThunk(
-  'NotificationCenter/MarkAsRead', 
+  'NotifyCenter/MarkAsRead',
   async (_, thunkAPI) => {
     try {
-      const response = await instence.get(`NotificationCenter/MarkAsRead`);
+      const response = await instence.put(`NotifyCenter/MarkAsRead`);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue({ error: error.message });
@@ -28,14 +28,14 @@ export const markNotificationsAsRead = createAsyncThunk(
 export const notificationUpdateSlice = createSlice({
   name: 'notificationUpdate',
   initialState: {
-    notificationUpdateList: [
-      {"ActionType": 0, "IpAddress": null, "ActionDate": "2023-04-06T10:55:17.5", "ID": 6, "SubAccountId": 0, "FileName": "63e7a610-72a1-4a6e-95c0-53f2b222bc52.CSV", "IsDeleted": false, "Status": 2, "CreationDate": "2023-04-06T10:55:17.5", "FileExtension": null, "NotifyEmail": null, "ActionName": "REQUEST", "StatusName": "READY" }, { "ActionType": 0, "IpAddress": null, "ActionDate": "2023-04-06T10:50:23.793", "ID": 5, "SubAccountId": 0, "FileName": "15d7a08d-8ee5-4182-95aa-f104abf63950.CSV", "IsDeleted": false, "Status": 2, "CreationDate": "2023-04-06T10:50:23.787", "FileExtension": null, "NotifyEmail": null, "ActionName": "REQUEST", "StatusName": "READY" }, { "ActionType": 0, "IpAddress": null, "ActionDate": "2023-04-05T11:03:08.26", "ID": 4, "SubAccountId": 0, "FileName": "4301db75-291a-4f38-a5ef-1649e6ea099e.CSV", "IsDeleted": false, "Status": 2, "CreationDate": "2023-04-05T11:03:08.26", "FileExtension": null, "NotifyEmail": null, "ActionName": "REQUEST", "StatusName": "READY" }, { "ActionType": 0, "IpAddress": null, "ActionDate": "2023-04-05T10:59:29.08", "ID": 3, "SubAccountId": 0, "FileName": "a3cf5099-530f-4f67-a93a-3a399b2784ff.CSV", "IsDeleted": false, "Status": 2, "CreationDate": "2023-04-05T10:59:29.07", "FileExtension": null, "NotifyEmail": null, "ActionName": "REQUEST", "StatusName": "READY" }, { "ActionType": 0, "IpAddress": null, "ActionDate": "2023-04-05T10:01:22.3", "ID": 6, "SubAccountId": 0, "FileName": "63e7a610-72a1-4a6e-95c0-53f2b222bc52.CSV", "IsDeleted": false, "Status": 2, "CreationDate": "2023-04-06T10:55:17.5", "FileExtension": null, "NotifyEmail": null, "ActionName": "REQUEST", "StatusName": "READY" }, { "ActionType": 0, "IpAddress": null, "ActionDate": "2023-04-05T09:59:10.603", "ID": 5, "SubAccountId": 0, "FileName": "15d7a08d-8ee5-4182-95aa-f104abf63950.CSV", "IsDeleted": false, "Status": 2, "CreationDate": "2023-04-06T10:50:23.787", "FileExtension": null, "NotifyEmail": null, "ActionName": "REQUEST", "StatusName": "READY" }, { "ActionType": 0, "IpAddress": null, "ActionDate": "2023-04-05T10:01:22.3", "ID": 6, "SubAccountId": 0, "FileName": "63e7a610-72a1-4a6e-95c0-53f2b222bc52.CSV", "IsDeleted": false, "Status": 2, "CreationDate": "2023-04-06T10:55:17.5", "FileExtension": null, "NotifyEmail": null, "ActionName": "REQUEST", "StatusName": "READY" }, { "ActionType": 0, "IpAddress": null, "ActionDate": "2023-04-05T09:59:10.603", "ID": 5, "SubAccountId": 0, "FileName": "15d7a08d-8ee5-4182-95aa-f104abf63950.CSV", "IsDeleted": false, "Status": 2, "CreationDate": "2023-04-06T10:50:23.787", "FileExtension": null, "NotifyEmail": null, "ActionName": "REQUEST", "StatusName": "READY" }, { "ActionType": 0, "IpAddress": null, "ActionDate": "2023-04-05T10:01:22.3", "ID": 6, "SubAccountId": 0, "FileName": "63e7a610-72a1-4a6e-95c0-53f2b222bc52.CSV", "IsDeleted": false, "Status": 2, "CreationDate": "2023-04-06T10:55:17.5", "FileExtension": null, "NotifyEmail": null, "ActionName": "REQUEST", "StatusName": "READY" }, { "ActionType": 0, "IpAddress": null, "ActionDate": "2023-04-05T09:59:10.603", "ID": 5, "SubAccountId": 0, "FileName": "15d7a08d-8ee5-4182-95aa-f104abf63950.CSV", "IsDeleted": false, "Status": 2, "CreationDate": "2023-04-06T10:50:23.787", "FileExtension": null, "NotifyEmail": null, "ActionName": "REQUEST", "StatusName": "READY" }
-    ]
+    notifyCenterList: null,
+    unreadMessages: 0
   },
   reducers: {},
   extraReducers: builder => {
     builder.addCase(getNotificationUpdates.fulfilled, (state, { payload }) => {
-      state.notificationUpdateList = payload;
+      state.notifyCenterList = payload?.Data;
+      state.unreadMessages = parseInt(payload?.Message);
     })
     builder.addCase(markNotificationsAsRead.fulfilled, (state, { payload }) => {
     })
