d775b0f41cfcefc09a95851ebae68c2c600017ba
Ido Shahar <idoshahar@gmail.com>
2023-04-24 17:42:15 +0300

    Add autoresponder redirect

--- src/screens/HtmlCampaign/CampaignEditorBee.js
+++ src/screens/HtmlCampaign/CampaignEditorBee.js
@@ -666,7 +666,7 @@ const CampaignEditor = ({ classes, ...props }) => {
           color="primary"
         >{t("common.save")}
         </Button>
-        <Button onClick={saveDesign}
+        {fromLink?.toLowerCase() !== 'autoresponder' && <Button onClick={saveDesign}
           variant='contained'
           size='medium'
           className={clsx(
