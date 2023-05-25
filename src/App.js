f8b404fc2bb3bb1a48e8fbe624d51b7e109dfbe9
ido@pulseem.com <ido@pulseem.com>
2023-05-25 08:42:24 +0300

    PR-1063 Added WA keys

--- src/App.js
+++ src/App.js
@@ -258,56 +258,56 @@ const renderRoutes = (classes, redirect) => {
 
       <Route
         path={whatsappRoutes.CREATE_TEMPLATE}
-        element={<WhatsappCreator classes={classes} />}
+        element={<WhatsappCreator classes={classes} key="wa-create" />}
       />
 
       <Route
         path={whatsappRoutes.CREATE_CAMPAIGN_PAGE1}
-        element={<SaveCampain classes={classes} />}
+        element={<SaveCampain classes={classes} key="wa-save-campaign" />}
       />
 
       <Route
         path={whatsappRoutes.CREATE_CAMPAIGN_PAGE2}
-        element={<SendCampaign classes={classes} />}
+        element={<SendCampaign classes={classes} key="wa-send" />}
       />
 
       <Route
         path={whatsappRoutes.TEMPLATE_MANAGEMENT}
-        element={<ManageWhatsAppTemplates classes={classes} />}
+        element={<ManageWhatsAppTemplates classes={classes}  key="wa-template-management" />}
       />
 
       <Route
         path={whatsappRoutes.REPORTS}
-        element={<WhatsappReports classes={classes} />}
+        element={<WhatsappReports classes={classes} key="wa-reports" />}
       />
 
       <Route
         path={whatsappRoutes.CAMPAIGN_MANAGEMENT}
-        element={<ManageWhatsAppCampaigns classes={classes} />}
+        element={<ManageWhatsAppCampaigns classes={classes} key="wa-CAMPAIGN_MANAGEMENT" />}
       />
 
       <Route
         path={whatsappRoutes.EDIT_TEMPLATE}
-        element={<WhatsappCreator classes={classes} />}
+        element={<WhatsappCreator classes={classes} key="wa-edit" />}
       />
 
       <Route
         path={whatsappRoutes.EDIT_CAMPAIGN_PAGE1}
-        element={<SaveCampain classes={classes} />}
+        element={<SaveCampain classes={classes} key="wa-save-campaign-page2" />}
       />
 
       <Route
         path={whatsappRoutes.EDIT_CAMPAIGN_PAGE2}
-        element={<SendCampaign classes={classes} />}
+        element={<SendCampaign classes={classes} key="wa-send-campaign-page2" />}
       />
 
       <Route
         path={whatsappRoutes.CHAT}
-        element={<WhatsappChat classes={classes} />}
+        element={<WhatsappChat classes={classes} key="wa-chate" />}
       />
       <Route
         path={whatsappRoutes.CHAT_CONVERSATION}
-        element={<WhatsappChat classes={classes} />}
+        element={<WhatsappChat classes={classes} key="wa-chat-conversation" />}
       />
 
       <Route
