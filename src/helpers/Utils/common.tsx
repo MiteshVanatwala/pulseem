72b3734eb5d5b46712261ab11d0c46a07db02da6
ido <ido@pulseem.com>
2023-06-11 10:36:16 +0300

    Fix templatesBySubAccountCategories reduce error

--- src/helpers/Utils/common.tsx
+++ src/helpers/Utils/common.tsx
@@ -15,7 +15,7 @@ export const getValueByFieldName = (
 };
 
 export const getUniqueValuesOfKey = (array: any, key: string) => {
-	return array.reduce(function (carry: any, item: any) {
+	return array?.reduce(function (carry: any, item: any) {
 		if (item[key] && !~carry.indexOf(item[key])) carry.push(item[key]);
 		return carry;
 	}, []);
