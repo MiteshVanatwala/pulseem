271e8e7a56c7462efc0a7f8f271697a7e0d18e95
ido <ido@pulseem.com>
2023-06-11 14:07:32 +0300

    Show SmsTotalMarketing

--- src/screens/Newsletter/Wizard/NewsletterSendSettings.js
+++ src/screens/Newsletter/Wizard/NewsletterSendSettings.js
@@ -1249,7 +1249,7 @@ const NewsletterSendSettings = ({ classes, ...props }) => {
                                             </Button>
                                         </Badge>
                                     </Stack>
-                                    {/* <Stack direction="row" justifyContent="center" alignItems="center">
+                                    <Stack direction="row" justifyContent="center" alignItems="center">
                                         <Badge variant="dot" color="primary" invisible={!smsMarketingIndication}>
                                             <Button
                                                 className={clsx(classes.actionButton, classes.actionButtonOutlinedBlue)}
@@ -1261,7 +1261,7 @@ const NewsletterSendSettings = ({ classes, ...props }) => {
                                                 {t("campaigns.newsLetterEditor.sendSettings.smsMarketing.title")}
                                             </Button>
                                         </Badge>
-                                    </Stack> */}
+                                    </Stack>
                                 </>
                             } />
                     </Grid>
