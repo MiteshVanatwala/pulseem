e61004ed18aebfd4a12e9a69f6f1e03fef638107
ido@pulseem.com <ido@pulseem.com>
2023-10-03 12:24:24 +0300

    Add Condition for base site domain

--- src/helpers/Routes/routes.tsx
+++ src/helpers/Routes/routes.tsx
@@ -20,30 +20,32 @@ import { FaBinoculars } from 'react-icons/fa';
 import { whatsappRoutes } from '../../screens/Whatsapp/Constant';
 import { logout } from "../Api/PulseemReactAPI";
 import { HiArrowRight } from "react-icons/hi";
-import { sitePrefix } from "../../config";
+import { sitePrefix, isProdMode } from "../../config";
 import { WhatsappIcon } from '../../assets/images/drawer/index';
+export const rootDomain = !isProdMode ? 'http://localhost:58123' : '/Pulseem/';
 
 export const getSettingsItem = (
   t: (text: string) => null | VoidFunction = () => null,
   style: string = '',
   isAllowSwitchAccount: Boolean = false,
   title: string = "Settings"
-) => ({
-  key: "settings",
-  title: title,
-  href: `${sitePrefix}AccountSettings`,
+  ) => ({
+    key: "settings",
+    title: title,
+    href: `${sitePrefix}AccountSettings`,
   options: [
     { key: 'accountSettings', title: t('master.RadMenuItemResource2.Text'), href: `${sitePrefix}AccountSettings`, iconSrc: SettingsMenuIcon, isShow: true },
-    { title: t('master.linkAccountBilling.Text'), href: '/Pulseem/AccountBilling.aspx?fromreact=true', iconSrc: DolarMenuIcon, isShow: true },
-    { title: t('master.RadMenuItemResource3.Text'), href: '/Pulseem/AccountUsers.aspx?fromreact=true', iconSrc: GroupMenuIcon, isShow: isAllowSwitchAccount },
-    { title: t('master.RadMenuItemResource4.Text'), href: '/Pulseem/AccountUsersReport.aspx?fromreact=true', iconSrc: GrafMenuIcon, isShow: isAllowSwitchAccount },
-    { title: t('master.RadMenuItemResource23.Text'), href: '/Pulseem/ExtraFieldsDefinition.aspx?fromreact=true', iconSrc: StarMenuIcon, isShow: true },
+    { title: t('master.linkAccountBilling.Text'), href: `${rootDomain}/AccountBilling.aspx?fromreact=true`, iconSrc: DolarMenuIcon, isShow: true },
+    { title: t('master.RadMenuItemResource3.Text'), href: `${rootDomain}/AccountUsers.aspx?fromreact=true`, iconSrc: GroupMenuIcon, isShow: isAllowSwitchAccount },
+    { title: t('master.RadMenuItemResource4.Text'), href: `${rootDomain}/AccountUsersReport.aspx?fromreact=true`, iconSrc: GrafMenuIcon, isShow: isAllowSwitchAccount },
+    { title: t('master.RadMenuItemResource23.Text'), href: `${rootDomain}/ExtraFieldsDefinition.aspx?fromreact=true`, iconSrc: StarMenuIcon, isShow: true },
     { title: t('master.linkApiSettingsResource1.Text'), href: `${sitePrefix}ApiSettings`, iconSrc: CodeMenuIcon, isShow: true },
     { key: 'SiteTracking', title: t('master.siteTracking'), href: `${sitePrefix}SiteTracking`, iconSrc: FaBinoculars, isFaIcon: true, isShow: true },
     { key: 'Integrations', title: t('integrations.title'), href: `${sitePrefix}Integrations`, iconSrc: SettingsMenuIcon, isShow: true },
     { title: t("appBar.logout"), onClick: logout, iconSrc: HiArrowRight, isFaIcon: true, isShow: true },
   ],
 });
+
 
 export const getRoutes = (
   t: (text: string) => null | VoidFunction = (par: string) => null,
@@ -78,22 +80,22 @@ export const getRoutes = (
         },
         {
           title: t("master.RadMenuItemResource7.Text"),
-          href: "/Pulseem/ClientSearch.aspx?fromreact=true",
+          href: `${rootDomain}/ClientSearch.aspx?fromreact=true`,
           isShow: true,
         },
         {
           title: t("master.RadMenuItemResource37.Text"),
-          href: "/Pulseem/ClientAdvancedSearch.aspx?fromreact=true",
+          href: `${rootDomain}/ClientAdvancedSearch.aspx?fromreact=true`,
           isShow: false,
         },
         {
           title: t("master.RadMenuItemResourceDynamicGroups.Text"),
-          href: "/Pulseem/DynamicGroups.aspx?fromreact=true",
+          href: `${rootDomain}/DynamicGroups.aspx?fromreact=true`,
           isShow: true,
         },
         {
           title: t("master.RadMenuItemResourceFileUploads.Text"),
-          href: "/Pulseem/FileUploads.aspx?fromreact=true",
+          href: `${rootDomain}/FileUploads.aspx?fromreact=true`,
           isShow: true,
         },
         {
@@ -126,27 +128,27 @@ export const getRoutes = (
         },
         {
           title: t("master.linkSendCampaignByResultResource1.Text"),
-          href: "/Pulseem/CampaignsByResults.aspx?fromreact=true",
+          href: `${rootDomain}/CampaignsByResults.aspx?fromreact=true`,
           isShow: false,
         },
         {
           title: t("master.linkAbTestingsResource1.Text"),
-          href: "/Pulseem/CampaignsAbTestings.aspx?fromreact=true",
+          href: `${rootDomain}/CampaignsAbTestings.aspx?fromreact=true`,
           isShow: false,
         },
         {
           title: t("master.RadMenuItemResource9a.Text"),
-          href: "/Pulseem/AutoSendPlans.aspx?fromreact=true",
+          href: `${rootDomain}/AutoSendPlans.aspx?fromreact=true`,
           isShow: true,
         },
         {
           title: t("master.RadMenuItemResource10.Text"),
-          href: "/Pulseem/CampaignTemplates.aspx?fromreact=true",
+          href: `${rootDomain}/CampaignTemplates.aspx?fromreact=true`,
           isShow: true,
         },
         {
           title: t("master.newslatterBasicEditor"),
-          href: "/Pulseem/CampaignEdit.aspx?NewsLetterType=Basic&fromreact=true",
+          href: `${rootDomain}/CampaignEdit.aspx?NewsLetterType=Basic&fromreact=true`,
           isShow: true,
         },
         {
@@ -189,7 +191,7 @@ export const getRoutes = (
         },
         {
           title: t("master.chatbotSMS"),
-          href: "/Pulseem/SMSSmartResponses.aspx?fromreact=true",
+          href: `${rootDomain}/SMSSmartResponses.aspx?fromreact=true`,
           isShow: true,
         },
         {
@@ -199,7 +201,7 @@ export const getRoutes = (
         },
         {
           title: t("master.NewMMSCampaign.Text"),
-          href: "/Pulseem/MmsCampaignEdit.aspx?fromreact=true",
+          href: `${rootDomain}/MmsCampaignEdit.aspx?fromreact=true`,
           isShow: true,
         },
         {
@@ -261,7 +263,7 @@ export const getRoutes = (
       options: [
         {
           title: t("master.RadMenuItemLandingPage.Text"),
-          href: "/Pulseem/LandingPageWizard.aspx?fromreact=true",
+          href: `${rootDomain}/LandingPageWizard.aspx?fromreact=true`,
           isShow: true,
         },
         {
@@ -271,7 +273,7 @@ export const getRoutes = (
         },
         {
           title: t("master.FormTemplatesResource1.Text"),
-          href: "/Pulseem/FormTemplates.aspx?fromreact=true",
+          href: `${rootDomain}/FormTemplates.aspx?fromreact=true`,
           isShow: true,
         },
       ],
@@ -287,7 +289,7 @@ export const getRoutes = (
       options: [
         {
           title: t("master.RadMenuItemCreateAutomationResource.Text"),
-          href: "/Pulseem/CreateAutomations.aspx?fromreact=true",
+          href: `${rootDomain}/CreateAutomations.aspx?fromreact=true`,
           isShow: true,
         },
         {
@@ -332,21 +334,21 @@ export const getRoutes = (
       isShow: true,
       icon: <img alt="Reports" src={ReportsIcon} />,
       options: [
-        { title: t('master.clalCollage'), href: '/Pulseem/ClalReport.aspx?fromreact=true', isShow: (isClalAccount === 'true' || isClalAccount === true) },
+        { title: t('master.clalCollage'), href: `${rootDomain}/ClalReport.aspx?fromreact=true`, isShow: (isClalAccount === 'true' || isClalAccount === true) },
         { title: t('master.RadMenuItemResource13.Text'), href: `${sitePrefix}reports/NewsletterReports`, isShow: true },
         { key: 'SmsReport', title: t('master.RadMenuItemResource24.Text'), href: `${sitePrefix}reports/SMSMainReport`, isShow: true },
         { key: 'MmsReport', title: t('mmsreport.mmsReport'), href: `${sitePrefix}Reports/MMSMainReport`, isShow: true },
         { key: 'whatsappReports', title: t('whatsapp.ReportsWhatsapp'), href: whatsappRoutes.REPORTS, isShow: true },
-        { title: t('master.AbTestsReport.Text'), href: '/Pulseem/AbTestsReport.aspx?fromreact=true', isShow: false },
-        { title: t('master.RadMenuItemResource15.Text'), href: '/Pulseem/AccountReport.aspx?fromreact=true', isShow: true },
-        { title: t('master.RadMenuItemResource16.Text'), href: '/Pulseem/CampaignComparison.aspx?fromreact=true', isShow: false },
+        { title: t('master.AbTestsReport.Text'), href: `${rootDomain}/AbTestsReport.aspx?fromreact=true`, isShow: false },
+        { title: t('master.RadMenuItemResource15.Text'), href: `${rootDomain}/AccountReport.aspx?fromreact=true`, isShow: true },
+        { title: t('master.RadMenuItemResource16.Text'), href: `${rootDomain}/CampaignComparison.aspx?fromreact=true`, isShow: false },
         { title: t('master.RadMenuItemResource18.Text'), href: `${sitePrefix}reports/Recipient`, isShow: true },
-        { title: t('master.RadMenuItemResource30.Text'), href: '/Pulseem/EmailAutoReports.aspx?fromreact=true', isShow: true },
-        { title: t('master.locRemovedReason.Text'), href: '/Pulseem/RemovedStats.aspx?fromreact=true', isShow: true },
+        { title: t('master.RadMenuItemResource30.Text'), href: `${rootDomain}/EmailAutoReports.aspx?fromreact=true`, isShow: true },
+        { title: t('master.locRemovedReason.Text'), href: `${rootDomain}/RemovedStats.aspx?fromreact=true`, isShow: true },
         { key: 'productsReport', title: t('report.ProductsReport.products'), href: `${sitePrefix}Reports/ProductsReport`, isShow: true },
         { key: 'directSendReport', title: t('report.DirectSendReport'), href:  `${sitePrefix}Reports/DirectSendReport`, isShow: true },
         { key: 'directSendReportArchive', title: t('report.ArchiveDirectSendReport'), href: `${sitePrefix}Reports/DirectSendReport/Archive`, isShow: true },
-        { title: t('master.OpenedClickedReport'), href: '/Pulseem/EmailCampaignStatistics.aspx?fromreact=true', isShow: true },
+        { title: t('master.OpenedClickedReport'), href: `${rootDomain}/EmailCampaignStatistics.aspx?fromreact=true`, isShow: true },
         { key: 'inboundMessages', title: t('master.responses'), href: `${sitePrefix}Reports/Inbound`, isShow: true },
         { key: 'recipientReport', title: t('common.recipient'), href: `${sitePrefix}Reports/recipient`, isShow: true },
       ],
