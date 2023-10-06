83bd85a1040cd5eb0e9ab6bd2d7888a99c6793fb
Mitesh Vanatwala <vmitesh.282@gmail.com>
2023-10-06 12:18:08 +0530

    PR-1539
    Adjust "Authentication" type templates to new WhatsApp Regulation

--- src/screens/Whatsapp/management/Types/Management.types.tsx
+++ src/screens/Whatsapp/management/Types/Management.types.tsx
@@ -115,3 +115,13 @@ export type UpdateWhatsappTierPayload = {
 export type UpdateWhatsappTier = {
 	payload: UpdateWhatsappTierPayload;
 };
+
+export type authenticationMockTemplateObject = {
+	body: string;
+	subtitle: string
+}
+export type AuthenticationMockTemplateType = {
+	[key: string]: authenticationMockTemplateObject;
+	authenticationEn: authenticationMockTemplateObject;
+	authenticationHebrew: authenticationMockTemplateObject;
+}
