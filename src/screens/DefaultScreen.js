b8f459afb228b8b07135c815c2b0ab972743e923
ido@pulseem.com <ido@pulseem.com>
2023-11-29 08:59:05 +0200

    Hide TAWK.TO due to overflow hidden iframe

--- src/screens/DefaultScreen.js
+++ src/screens/DefaultScreen.js
@@ -31,28 +31,28 @@ const DefaultScreen = ({ classes, children, currentPage = '', subPage = '', cont
 
   title = title ? `${title} | ${t('master.pulseemSystem')}` : t('master.pulseemSystem');
 
-  useEffect(() => {
-    if (process.env.REACT_APP_MODE === "PROD") {
-      const liveChat = document.createElement("script");
-      liveChat.type = 'text/javascript';
-      liveChat.async = true;
-      liveChat.innerHTML = `
-          var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-          (function () {
-              var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-              s1.async = true;
-              s1.src = 'https://embed.tawk.to/59c8caaa4854b82732ff1f7d/default';
-              s1.charset = 'UTF-8';
-              s1.setAttribute('crossorigin', '*');
-              s0.parentNode.insertBefore(s1, s0);
-          })();`;
-      document.body.append(liveChat)
+  // useEffect(() => {
+  //   if (process.env.REACT_APP_MODE === "PROD") {
+  //     const liveChat = document.createElement("script");
+  //     liveChat.type = 'text/javascript';
+  //     liveChat.async = true;
+  //     liveChat.innerHTML = `
+  //         var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+  //         (function () {
+  //             var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+  //             s1.async = true;
+  //             s1.src = 'https://embed.tawk.to/59c8caaa4854b82732ff1f7d/default';
+  //             s1.charset = 'UTF-8';
+  //             s1.setAttribute('crossorigin', '*');
+  //             s0.parentNode.insertBefore(s1, s0);
+  //         })();`;
+  //     document.body.append(liveChat)
 
-      return () => {
-        document.body.removeChild(liveChat);
-      }
-    }
-  }, [])
+  //     return () => {
+  //       document.body.removeChild(liveChat);
+  //     }
+  //   }
+  // }, [])
 
 
   return (
