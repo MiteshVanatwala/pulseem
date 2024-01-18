e55cf9b9d957674274e092c3ac547c55d0b454f8
Mitesh Vanatwala <vmitesh.282@gmail.com>
2024-01-18 12:31:25 +0530

    PR-1635
    Implemented new Grid in BulkStatus

--- src/components/Balance/BulkStatus.js
+++ src/components/Balance/BulkStatus.js
@@ -17,7 +17,7 @@ import useRedirect from '../../helpers/Routes/Redirect';
 import { sitePrefix } from '../../config';
 
 const BulkStatus = ({ classes }) => {
-  const { billingTypeId, isRTL } = useSelector(state => state.core)
+  const { billingTypeId, windowSize, isRTL } = useSelector(state => state.core)
   const { accountSettings, accountFeatures } = useSelector(state => state.common);
   const { packagesDetails, accountAvailablePackages } = useSelector(state => state.dashboard);
   const [isOpenPackageDialog, setIsOpenPackageDialog] = useState(false);
@@ -179,12 +179,12 @@ const BulkStatus = ({ classes }) => {
             className={clsx(classes.flex, classes.mt2, classes.mb2, classes.paddingSides15)}
             justifyContent='space-between'
           >
-            <Grid item md={5}>
+            <Grid item md={5} xs={6}>
               <SmsIcon className={classes.shoppingCartIcon} />
               <Typography className={clsx(classes.bulkTitle)}>{t('appBar.sms.title')}</Typography>
             </Grid>
 
-            <Grid item md={3} className={classes.paddingSides10}>
+            <Grid item md={3} xs={6} className={clsx(classes.paddingSides10, windowSize === 'xs' ? classes.textRight : '')}>
               <Typography className={clsx(classes.bold)}
                 title={`${getBillingTypeText(Sms)} ${t('report.Credits')}`}
                 aria-label={`${getBillingTypeText(Sms)} ${t('report.Credits')}`}>
@@ -192,7 +192,7 @@ const BulkStatus = ({ classes }) => {
               </Typography>
             </Grid>
 
-            <Grid item md={4} className={isRTL ? classes.textLeft : classes.textRight}>
+            <Grid item md={4} xs={12} className={isRTL ? classes.textLeft : classes.textRight}>
               {
                 isAllowSms() && (
                   <Button className={clsx(classes.btn, classes.btnRounded, classes.f12)} onClick={() => showPackageDialogType({ type: 3, title: t('common.smsBulkTitle') })}>
@@ -210,12 +210,12 @@ const BulkStatus = ({ classes }) => {
             className={clsx(classes.flex, classes.mt2, classes.mb2, classes.paddingSides15)}
             justifyContent='space-between'
           >
-            <Grid item md={5}>
+            <Grid item md={5} xs={6}>
               <NewsletterIcon className={classes.shoppingCartIcon} />
               <Typography className={classes.bulkTitle}>{t('appBar.newsletter.title')}</Typography>
             </Grid>
 
-            <Grid item md={3} className={classes.paddingSides10}>
+            <Grid item md={3} xs={6} className={clsx(classes.paddingSides10, windowSize === 'xs' ? classes.textRight : '')}>
               <Typography
                 className={clsx(classes.bold)}
                 title={`${getBillingTypeText(Newsletters)} ${t('report.Credits')}`}
@@ -242,12 +242,12 @@ const BulkStatus = ({ classes }) => {
             className={clsx(classes.flex, classes.mt2, classes.mb2, classes.paddingSides15)}
             justifyContent='space-between'
           >
-            <Grid item md={5}>
+            <Grid item md={5} xs={12}>
               <BellIcon className={classes.shoppingCartIcon} />
               <Typography className={classes.bulkTitle}>{t('master.notifications')}</Typography>
             </Grid>
 
-            <Grid item md={7} className={isRTL ? classes.textLeft : classes.textRight}>
+            <Grid item md={7} xs={12} className={isRTL ? classes.textLeft : classes.textRight}>
               <Button className={clsx(classes.btn, classes.btnRounded, classes.f12)} onClick={() => Redirect({ url: `${sitePrefix}Notifications` })}>
                 {t('dashboard.freeTrial')}
                 {isRTL ? <MdArrowBackIos /> : <MdArrowForwardIos />}
@@ -262,18 +262,18 @@ const BulkStatus = ({ classes }) => {
               className={clsx(classes.flex, classes.mt2, classes.mb2, classes.paddingSides15)}
               justifyContent='space-between'
             >
-              <Grid item md={5}>
+              <Grid item md={5} xs={6}>
                 <WhatsappIcon className={classes.shoppingCartIcon} />
                 <Typography className={classes.bulkTitle}>{t('appBar.whatsapp.title')}</Typography>
               </Grid>
 
-              <Grid item md={3} className={classes.paddingSides10}>
+              <Grid item md={3} xs={6} className={clsx(classes.paddingSides10, windowSize === 'xs' ? classes.textRight : '')}>
                 <Typography className={clsx(classes.bold)}>
                   {billingTypeId === "1" ? t('dashboard.perUsage') : `${getBillingTypeText(Whatsapp)} ${t('common.NIS')}`}
                 </Typography>
               </Grid>
 
-              <Grid item md={4} className={isRTL ? classes.textLeft : classes.textRight}>
+              <Grid item md={4} xs={12} className={isRTL ? classes.textLeft : classes.textRight}>
                 <Button className={clsx(classes.btn, classes.btnRounded, classes.f12)} onClick={() => showPackageDialogType(4)}>
                   {t('dashboard.purchase')}
                   {isRTL ? <MdArrowBackIos /> : <MdArrowForwardIos />}
