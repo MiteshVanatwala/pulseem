58534aad1630ec14f419d08b805cd2172bc5312e
Mitesh Vanatwala <vmitesh.282@gmail.com>
2024-02-07 16:27:03 +0530

    Issue Fixes

--- src/Models/APIResponse.ts
+++ src/Models/APIResponse.ts
@@ -1,7 +1,5 @@
-import { LandingPageModel } from "./LandingPage/LandingPage";
-
 export interface PulseemResponse {
     StatusCode: number;
     Message: string;
-    Data: LandingPageModel;
+    Data: any;
 }
