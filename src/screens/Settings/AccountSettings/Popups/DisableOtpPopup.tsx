af1edacc20cdb7a7c90d78ab45b850846a64f184
ido@pulseem.com <ido@pulseem.com>
2024-03-31 17:04:05 +0300

    Started implemeneted Sending Code logic

--- src/screens/Settings/AccountSettings/Popups/DisableOtpPopup.tsx
+++ src/screens/Settings/AccountSettings/Popups/DisableOtpPopup.tsx
@@ -1,32 +1,39 @@
 import { useTranslation } from "react-i18next";
 import { BaseDialog } from "../../../../components/DialogTemplates/BaseDialog";
 import { RenderHtml } from "../../../../helpers/Utils/HtmlUtils";
-import { Grid, Typography } from "@material-ui/core";
+import { FormControl, Grid, MenuItem, Select, Typography } from "@material-ui/core";
 import clsx from 'clsx';
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getAllTwoFactorAuthValues } from "../../../../redux/reducers/AccountSettingsSlice";
 import PulseemRadio from "../../../../components/Controlls/PulseemRadio";
+import { IoIosArrowDown } from "react-icons/io";
+import { StateType } from "../../../../Models/StateTypes";
 
 const DisableOtpPopup = ({ classes, onClose, onConfirm }: any) => {
+  const { isRTL } = useSelector((state: StateType) => state.core);
   const { t } = useTranslation();
   const [emailList, setEmailList] = useState([]);
   const [cellphoneList, setCellphoneList] = useState([]);
   const [selectedOption, setSelected] = useState<string>('1');
+  const [authSelected, setAuthSelected] = useState<string>(t('common.select'));
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     const getValues = async () => {
       const vals = await dispatch(getAllTwoFactorAuthValues()) as any;
-      setEmailList(vals?.payload?.Emails)
-      setCellphoneList(vals?.payload?.Cellphones)
+      setEmailList(vals?.payload?.Data?.Emails)
+      setCellphoneList(vals?.payload?.Data?.Cellphones)
     }
 
     getValues();
 
   }, [])
-  // const { }
+
+  const handleSendCode = () => {
+
+  }
 
   return <BaseDialog
     // icon={<MdCelebration />}
@@ -44,6 +51,7 @@ const DisableOtpPopup = ({ classes, onClose, onConfirm }: any) => {
             isVerical={false}
             name={'otpType'}
             onChange={(e: any, x: any, y: any) => {
+              setAuthSelected(t('common.select'))
               setSelected(e.target.value.toString())
             }}
             radioOptions={[
@@ -63,8 +71,58 @@ const DisableOtpPopup = ({ classes, onClose, onConfirm }: any) => {
             value={selectedOption}
           />
         </Grid>
-        <Grid item md={6} xs={12}>
-          {/* List of emails / cellphone by selected type */}
+        <Grid item md={12} xs={12}>
+          <FormControl
+            variant='standard'
+            className={clsx(classes.selectInputFormControl, classes.w100, classes.mt15)}
+            style={{ maxWidth: 300 }}>
+            <Select
+              variant="standard"
+              // disabled
+              autoWidth
+              value={authSelected || ''}
+              name='TwoFactorAuthOptionID'
+              onChange={(e: any) =>
+                setAuthSelected(e.target.value)
+              }
+              IconComponent={() => <IoIosArrowDown size={20} className={classes.dropdownIconComponent} />}
+              MenuProps={{
+                PaperProps: {
+                  style: {
+                    maxHeight: 300,
+                    direction: isRTL ? 'rtl' : 'ltr'
+                  },
+                },
+              }}
+            >
+              <MenuItem
+                key={''}
+                value={t('common.select')}
+              >
+                {t('common.select')}
+              </MenuItem>
+              {selectedOption === '1' && emailList?.map((item: any, index) => {
+                return (
+                  <MenuItem
+                    key={index}
+                    value={item?.AuthValue}
+                  >
+                    {t(item?.AuthValue)}
+                  </MenuItem>
+                );
+              })}
+              {selectedOption === '2' && cellphoneList?.map((item: any, index) => {
+                return (
+                  <MenuItem
+                    key={index}
+                    value={item?.AuthValue}
+                  >
+                    {t(item?.AuthValue)}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
         </Grid>
       </Grid>
       <Typography className={clsx(classes.font16, classes.mt5)}>
@@ -79,7 +137,8 @@ const DisableOtpPopup = ({ classes, onClose, onConfirm }: any) => {
     onCancel={() => onClose()}
     onClose={() => onClose()}
     onConfirm={() => {
-      onClose();
+      handleSendCode();
+      // onClose();
       // handleConfirmOtpRegulation();
     }}
     showDefaultButtons={true}
