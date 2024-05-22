7b1e5ba235563dadb7acb90499db3a052b28f11f
Mitesh Vanatwala <vmitesh.282@gmail.com>
2024-05-22 12:03:22 +0530

    Signup UI changes

--- src/helpers/Utils/HtmlUtils.tsx
+++ src/helpers/Utils/HtmlUtils.tsx
@@ -1,3 +1,5 @@
+import { makeStyles } from "@material-ui/core";
+
 export const RenderHtml = (html: any) => {
     function createMarkup() {
         return { __html: html };
@@ -14,3 +16,15 @@ export const ConvertObjectToQueryString = (object: any) => {
     });
     return queryString;
 }
+
+export const useStylesBootstrapPasswordHint = makeStyles((theme) => ({
+    arrow: {
+      color: theme.palette.common.black,
+    },
+    tooltip: {
+      backgroundColor: theme.palette.common.black,
+      fontSize: 14,
+      width: 350,
+      maxWidth: "none",
+    },
+}));
