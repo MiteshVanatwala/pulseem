e3df6e8d9afd84032489d7c12eb004f87af5ed00
ido@pulseem.com <ido@pulseem.com>
2024-07-24 17:03:24 +0300

    title & emails implementation. next -> submit & results PR-1959

--- src/screens/Newsletter/AMP/AmpRegistration.tsx
+++ src/screens/Newsletter/AMP/AmpRegistration.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx'
 import DefaultScreen from '../../DefaultScreen';
-import { Box, FormControl, Select, Typography } from '@material-ui/core';
+import { Box, Checkbox, FormControl, ListItemText, MenuItem, Select, Typography } from '@material-ui/core';
 import { BaseDialog } from '../../../components/DialogTemplates/BaseDialog';
 import { Loader } from '../../../components/Loader/Loader';
 import { useEffect, useState } from 'react';
@@ -11,10 +11,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { StateType } from '../../../Models/StateTypes';
 import { IoIosArrowDown } from 'react-icons/io';
 import { PulseemFeatures } from '../../../model/PulseemFields/Fields';
+import { Title } from '../../../components/managment/Title';
 
 const AmpRegistration = ({ classes }: any) => {
     const [showLoader, setShowLoader] = useState<boolean>(true);
-    const [selectedEmail, setSelectedEmail] = useState<string>('');
+    const [selectedEmail, setSelectedEmail] = useState<string[]>([]);
     const { t } = useTranslation();
     const dispatch = useDispatch();
     const { verifiedEmails, accountSettings, accountFeatures } = useSelector((state: StateType) => state.common);
@@ -35,89 +36,96 @@ const AmpRegistration = ({ classes }: any) => {
             customPadding={true}
             containerClass={clsx(classes.mb50, classes.editorCont)}
         >
-            <Box className={"containerBody"}>
-                select email to verify amp
-                <Box className='selectWrapper'>
-                    <FormControl
-                        className={clsx(classes.selectInputFormControl, classes.w100)}
-                    >
-                        <Select
-                            native
-                            variant="standard"
-                            name="FromEmail"
-                            value={selectedEmail}
-                            className={clsx(classes.pbt5, classes.fromEmailSelect)}
-                            onChange={(event: any) => {
-                                setSelectedEmail(event?.target?.value);
-                            }}
-                            IconComponent={() => <IoIosArrowDown size={20} className={classes.dropdownIconComponent} />}
-                            MenuProps={{
-                                PaperProps: {
-                                    style: {
-                                        maxHeight: 300,
-                                    },
-                                },
-                            }}
+            <Box className={classes.mb50}>
+                <Box className={'topSection'}>
+                    <Title Text={t('master.ampRegistration')} classes={classes} />
+                </Box>
+                <Box className={"containerBody"}>
+                    <Typography>{t('campaigns.selectUptoFiveEmails')}</Typography>
+                    <Box className='selectWrapper'>
+                        <FormControl
+                            className={clsx(classes.selectInputFormControl, classes.w100)}
                         >
-                            <option
-                                key='-1'
-                                value='-1'
-                                disabled
+                            <Select
+                                renderValue={() => {
+                                    return selectedEmail?.join(',')
+                                }}
+                                multiple
+                                variant="standard"
+                                name="FromEmail"
+                                value={selectedEmail}
+                                className={clsx(classes.pbt5, classes.fromEmailSelect)}
+                                onChange={(event: any) => {
+                                    if (event?.target?.value.length <= 5) {
+                                        setSelectedEmail(event?.target?.value);
+                                    }
+                                }}
+                                IconComponent={() => <IoIosArrowDown size={20} className={classes.dropdownIconComponent} />}
+                                MenuProps={{
+                                    PaperProps: {
+                                        style: {
+                                            maxHeight: 300,
+                                        },
+                                    },
+                                }}
                             >
-                                {t("common.select")}
-                            </option>
-                            {verifiedEmails?.filter((email) => { return email.IsVerified === true }).map((item: any, index: number) => {
-                                return <option
-                                    key={index}
-                                    value={item.Number}
-                                    name={item.Number}
+                                <option
+                                    key='-1'
+                                    value='-1'
+                                    disabled
                                 >
-                                    {t(item.Number)}
+                                    {t("common.select")}
                                 </option>
-                            })}
-                            {accountFeatures?.indexOf(PulseemFeatures.HIDE_SHARED_DOMAIN) === -1 && accountSettings?.SubAccountSettings?.SharedEmailDomain && <option
-                                key={verifiedEmails.length + 1}
-                                value={accountSettings?.SubAccountSettings?.SharedEmailDomain}
-                                name={accountSettings?.SubAccountSettings?.SharedEmailDomain}
-                            >
-                                {/* <ListItemIcon style={{ minWidth: 25 }}>
+                                {verifiedEmails?.filter((email: any) => { return email.IsVerified === true }).map((item: any, index: number) => {
+                                    return <MenuItem key={index} value={item.Number} style={{ paddingRight: 15 }}>
+                                        <Checkbox checked={selectedEmail.indexOf(item.Number) > -1} disabled={selectedEmail.indexOf(item.Number) === -1 && selectedEmail.length > 4} />
+                                        <ListItemText primary={item.Number} />
+                                    </MenuItem>
+                                })}
+                                {accountFeatures?.indexOf(PulseemFeatures.HIDE_SHARED_DOMAIN) === -1 && accountSettings?.SubAccountSettings?.SharedEmailDomain && <option
+                                    key={verifiedEmails.length + 1}
+                                    value={accountSettings?.SubAccountSettings?.SharedEmailDomain}
+                                    name={accountSettings?.SubAccountSettings?.SharedEmailDomain}
+                                >
+                                    {/* <ListItemIcon style={{ minWidth: 25 }}>
                                                 <MdOutlineVerified style={{ color: 'green', fontSize: 20 }} title={t('common.domainVerification.verifiedDomain')} />
                                             </ListItemIcon> */}
-                                {t(accountSettings?.SubAccountSettings?.SharedEmailDomain)}
-                            </option>}
-                        </Select>
-                    </FormControl>
-                </Box>
-
-                <Box className={classes.flex} style={{ justifyContent: 'end', marginTop: 15 }}>
-                    <WizardActions
-                        classes={classes}
-                        onBack={{
-                            callback: () => console.log('back')
-                        }}
-                        onDelete={() => { console.log('back') }}
-                        additionalButtons={() => { console.log('back') }}
-                        additionalButtonsOnStart={() => { console.log('back') }}
-                    />
-                </Box>
-                <BaseDialog
-                    classes={classes}
-                    open={false}
-                    title={t("campaigns.GridButtonColumnResource2.ConfirmTitle")}
-                    showDivider={true}
-                    onClose={() => { console.log('open') }}
-                    onCancel={() => { console.log('open') }}
-                    onConfirm={() => { console.log('open') }}
-                    cancelText="common.Cancel"
-                    confirmText="common.Ok"
-                >
-                    <Box>
-                        <Typography variant="subtitle1">
-                            {t("campaigns.GridButtonColumnResource2.ConfirmText")}
-                        </Typography>
+                                    {t(accountSettings?.SubAccountSettings?.SharedEmailDomain)}
+                                </option>}
+                            </Select>
+                        </FormControl>
                     </Box>
-                </BaseDialog>
-                <Loader isOpen={showLoader} />
+
+                    {/* <Box className={classes.flex} style={{ justifyContent: 'end', marginTop: 15 }}>
+                        <WizardActions
+                            classes={classes}
+                            onBack={{
+                                callback: () => console.log('back')
+                            }}
+                            onDelete={() => { console.log('back') }}
+                            additionalButtons={() => { console.log('back') }}
+                            additionalButtonsOnStart={() => { console.log('back') }}
+                        />
+                    </Box> */}
+                    <BaseDialog
+                        classes={classes}
+                        open={false}
+                        title={t("campaigns.GridButtonColumnResource2.ConfirmTitle")}
+                        showDivider={true}
+                        onClose={() => { console.log('open') }}
+                        onCancel={() => { console.log('open') }}
+                        onConfirm={() => { console.log('open') }}
+                        cancelText="common.Cancel"
+                        confirmText="common.Ok"
+                    >
+                        <Box>
+                            <Typography variant="subtitle1">
+                                {t("campaigns.GridButtonColumnResource2.ConfirmText")}
+                            </Typography>
+                        </Box>
+                    </BaseDialog>
+                    <Loader isOpen={showLoader} />
+                </Box>
             </Box>
         </DefaultScreen>
     )
