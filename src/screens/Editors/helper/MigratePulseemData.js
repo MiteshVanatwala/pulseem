37e208d8bf13b51efdb6e7640c2c2b1a7ff270e7
ido@pulseem.com <ido@pulseem.com>
2024-10-27 09:46:04 +0200

    remove field direction

--- src/screens/Editors/helper/MigratePulseemData.js
+++ src/screens/Editors/helper/MigratePulseemData.js
@@ -44,37 +44,37 @@ export const initLandingPages = (previousLandingData, t) => {
 export const initClientForm = (extraFields, t, isRTL) => {
     return new Promise((resolve, reject) => {
         try {
-            const fieldDirection = { dir: isRTL ? 'rtl' : 'ltr' };
+            // const fieldDirection = { dir: isRTL ? 'rtl' : 'ltr' };
             const clientForm = {
-                FirstName: new BeeFormModel(ElementTypes.text, t('common.first_name'), false, { ...fieldDirection, placeholder: t('landingPages.placeholder.first_name') }),
-                LastName: new BeeFormModel(ElementTypes.text, t('common.last_name'), false, { ...fieldDirection, placeholder: t('landingPages.placeholder.last_name') }),
-                Email: new BeeFormModel(ElementTypes.text, t('common.email'), false, { ...fieldDirection, maxlength: '100', required: 'true', placeholder: t('landingPages.placeholder.email') }),
-                Cellphone: new BeeFormModel(ElementTypes.text, t('common.cellphone'), false, { ...fieldDirection, pattern: '\\d*', required: 'true', placeholder: t('landingPages.placeholder.cellphone') }),
-                Zip: new BeeFormModel(ElementTypes.text, t('common.zip'), true, { ...fieldDirection, maxlength: '50', pattern: '\\d*', placeholder: t('landingPages.placeholder.zip') }),
-                City: new BeeFormModel(ElementTypes.text, t('common.city'), true, { ...fieldDirection, maxlength: '100', placeholder: t('landingPages.placeholder.city') }),
-                State: new BeeFormModel(ElementTypes.text, t('common.state'), true, { ...fieldDirection, maxlength: '100', placeholder: t('landingPages.placeholder.state') }),
-                Company: new BeeFormModel(ElementTypes.text, t('common.company'), true, { ...fieldDirection, maxlength: '100', placeholder: t('landingPages.placeholder.company') }),
-                Country: new BeeFormModel(ElementTypes.text, t('common.country'), true, { ...fieldDirection, maxlength: '100', placeholder: t('landingPages.placeholder.country') }),
-                Address: new BeeFormModel(ElementTypes.text, t('common.address'), true, { ...fieldDirection, maxlength: '100', placeholder: t('landingPages.placeholder.address') }),
-                Telephone: new BeeFormModel(ElementTypes.text, t('common.telephone'), true, { ...fieldDirection, size: '100', pattern: '\\d*', placeholder: t('landingPages.placeholder.telephone') }),
-                BirthDate: new BeeFormModel(ElementTypes.text, t('common.birth_date'), true, { ...fieldDirection, placeholder: t('landingPages.placeholder.birth_date') }, 'frm-1 bee-date'),
-                ReminderDate: new BeeFormModel(ElementTypes.text, t('common.reminder_date'), true, { ...fieldDirection, placeholder: t('landingPages.placeholder.reminder_date') }, 'frm-1 bee-date'),
-                PulseemSurvey1: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion'), true, { ...fieldDirection, required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey1" }, 'PulseemSurvey'),
-                PulseemSurvey2: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion2'), true, { ...fieldDirection, required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey2" }, 'PulseemSurvey'),
-                PulseemSurvey3: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion3'), true, { ...fieldDirection, required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey3" }, 'PulseemSurvey'),
-                PulseemSurvey4: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion4'), true, { ...fieldDirection, required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey4" }, 'PulseemSurvey'),
-                PulseemSurvey5: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion5'), true, { ...fieldDirection, required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey5" }, 'PulseemSurvey'),
-                PulseemSurvey6: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion6'), true, { ...fieldDirection, required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey6" }, 'PulseemSurvey'),
-                PulseemSurvey7: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion7'), true, { ...fieldDirection, required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey7" }, 'PulseemSurvey'),
-                PulseemSurvey8: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion8'), true, { ...fieldDirection, required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey8" }, 'PulseemSurvey'),
-                PulseemSurvey9: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion9'), true, { ...fieldDirection, required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey9" }, 'PulseemSurvey'),
-                PulseemSurvey10: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion10'), true, { ...fieldDirection, required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey10" }, 'PulseemSurvey')
+                FirstName: new BeeFormModel(ElementTypes.text, t('common.first_name'), false, {  placeholder: t('landingPages.placeholder.first_name') }),
+                LastName: new BeeFormModel(ElementTypes.text, t('common.last_name'), false, {  placeholder: t('landingPages.placeholder.last_name') }),
+                Email: new BeeFormModel(ElementTypes.text, t('common.email'), false, {  maxlength: '100', required: 'true', placeholder: t('landingPages.placeholder.email') }),
+                Cellphone: new BeeFormModel(ElementTypes.text, t('common.cellphone'), false, {  pattern: '\\d*', required: 'true', placeholder: t('landingPages.placeholder.cellphone') }),
+                Zip: new BeeFormModel(ElementTypes.text, t('common.zip'), true, {  maxlength: '50', pattern: '\\d*', placeholder: t('landingPages.placeholder.zip') }),
+                City: new BeeFormModel(ElementTypes.text, t('common.city'), true, {  maxlength: '100', placeholder: t('landingPages.placeholder.city') }),
+                State: new BeeFormModel(ElementTypes.text, t('common.state'), true, {  maxlength: '100', placeholder: t('landingPages.placeholder.state') }),
+                Company: new BeeFormModel(ElementTypes.text, t('common.company'), true, {  maxlength: '100', placeholder: t('landingPages.placeholder.company') }),
+                Country: new BeeFormModel(ElementTypes.text, t('common.country'), true, {  maxlength: '100', placeholder: t('landingPages.placeholder.country') }),
+                Address: new BeeFormModel(ElementTypes.text, t('common.address'), true, {  maxlength: '100', placeholder: t('landingPages.placeholder.address') }),
+                Telephone: new BeeFormModel(ElementTypes.text, t('common.telephone'), true, {  size: '100', pattern: '\\d*', placeholder: t('landingPages.placeholder.telephone') }),
+                BirthDate: new BeeFormModel(ElementTypes.text, t('common.birth_date'), true, {  placeholder: t('landingPages.placeholder.birth_date') }, 'frm-1 bee-date'),
+                ReminderDate: new BeeFormModel(ElementTypes.text, t('common.reminder_date'), true, {  placeholder: t('landingPages.placeholder.reminder_date') }, 'frm-1 bee-date'),
+                PulseemSurvey1: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion'), true, {  required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey1" }, 'PulseemSurvey'),
+                PulseemSurvey2: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion2'), true, {  required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey2" }, 'PulseemSurvey'),
+                PulseemSurvey3: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion3'), true, {  required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey3" }, 'PulseemSurvey'),
+                PulseemSurvey4: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion4'), true, {  required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey4" }, 'PulseemSurvey'),
+                PulseemSurvey5: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion5'), true, {  required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey5" }, 'PulseemSurvey'),
+                PulseemSurvey6: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion6'), true, {  required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey6" }, 'PulseemSurvey'),
+                PulseemSurvey7: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion7'), true, {  required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey7" }, 'PulseemSurvey'),
+                PulseemSurvey8: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion8'), true, {  required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey8" }, 'PulseemSurvey'),
+                PulseemSurvey9: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion9'), true, {  required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey9" }, 'PulseemSurvey'),
+                PulseemSurvey10: new BeeFormModel(ElementTypes.text, t('landingPages.surveyQuestion10'), true, {  required: 'true', placeholder: t('landingPages.placeholder.surveyQuestion'), name: "PulseemSurvey10" }, 'PulseemSurvey')
             }
             if (extraFields && Object.keys(extraFields)?.length > 0) {
                 Object.keys(extraFields).forEach((key) => {
                     const val = extraFields[key];
                     if (val !== '') {
-                        clientForm[key] = new BeeFormModel(ElementTypes.text, val, true, { ...fieldDirection, placeholder: t('landingPages.placeholder.typeHere') + ' ' + val, name: key }, key?.toLowerCase()?.indexOf('date') > -1 && 'frm-1 bee-date');
+                        clientForm[key] = new BeeFormModel(ElementTypes.text, val, true, {  placeholder: t('landingPages.placeholder.typeHere') + ' ' + val, name: key }, key?.toLowerCase()?.indexOf('date') > -1 && 'frm-1 bee-date');
                     }
                 });
             }
