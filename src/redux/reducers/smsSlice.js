d1fcee5f25a8d36d975a61cd2d5cde319d146ab3
ido@pulseem.com <ido@pulseem.com>
2024-11-03 13:29:40 +0200

    cleanup

--- src/redux/reducers/smsSlice.js
+++ src/redux/reducers/smsSlice.js
@@ -467,7 +467,6 @@ export const smsSlice = createSlice({
 		previousLandingData: [],
 		previousCampaignData: [],
 		extraData: [],
-		// accountId: [],
 		subAccountGroups: [],
 		getCampaignSum: [],
 		finishedCampaigns: [],
@@ -555,7 +554,6 @@ export const smsSlice = createSlice({
 			for (let i = 0; i < payload.length; i++) {
 				tempArr.push({ ...payload[i], selected: false });
 			}
-			// state.accountId = tempArr;
 			state.subAccountGroups = tempArr;
 		});
 		builder.addCase(getSMSDirectReport.rejected, (state, action) => {
