fd5a43b689e651a690a326e83a9dc6d6b6d5bd59
ido@pulseem.com <ido@pulseem.com>
2025-01-08 16:04:15 +0200

    PR-2175

---
+++ src/redux/reducers/SubUserSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { PulseemReactInstance } from "../../helpers/Api/PulseemReactAPI";
+import { PulseemResponse } from "../../Models/APIResponse";
+import { SubUserModel } from "../../Models/SubUser/SubUsers";
+
+export const getAllUsers = createAsyncThunk(
+  'SubUser/GetAllUsers',
+  async (_, thunkAPI) => {
+    try {
+      const response = await PulseemReactInstance.get(`SubUser/GetAllUsers`);
+      return response.data as PulseemResponse;
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue({ error: error.message });
+    }
+  }
+);
+
+
+export const SubUserSlice = createSlice({
+  name: "SubUser",
+  initialState: {
+    subUsers: [] as SubUserModel[]
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getAllUsers.fulfilled, (state, { payload }) => {
+      state.subUsers = payload?.Data?.Items || [];
+    });
+  },
+});
+
+export default SubUserSlice.reducer;
