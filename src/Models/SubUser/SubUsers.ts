0326b4b6efc47dfb0eb086a806e2d87eda32cbd1
ido@pulseem.com <ido@pulseem.com>
2025-01-08 16:04:47 +0200

    PR-2176

---
+++ src/Models/SubUser/SubUsers.ts
@@ -0,0 +1,37 @@
+export interface SubUserModel {
+    AspnetUserId: string;
+    FirstName: string;
+    LastName: string;
+    Cellphone: string;
+    Email: string;
+    UserName: string;
+    Password: string;
+    IsDeleted: boolean;
+    IsApproved: boolean;
+    UpdatedDate: string | null;
+    CreationDate: string | null;
+    SubUserPermissions: string;
+    UserPermissionsList: eSubUserPermissions[];
+}
+
+export enum eSubUserPermissions {
+    AllowSend = 1,
+    AllowExport = 2,
+    AllowDelete = 3,
+    AllowSubUsers = 4,
+    HideRecipietns = 5
+}
+
+export interface SubUserChangeLog {
+    ID: number;
+    UserId: number;
+    OldPermssions: string;
+    NewPermssions: string;
+    CreatedDate: string;
+    IpAddress: string;
+}
+
+export interface SubUserToPermissions {
+    SubIserId: number;
+    SubUserPermission: number[];
+}
