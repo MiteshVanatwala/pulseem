7f79a71b785c41a78a7e6baffd64ea6adce150b5
ido@pulseem.com <ido@pulseem.com>
2025-03-30 10:07:19 +0300

    3 - small translation files

--- src/assets/translations/pl/index.js
+++ src/assets/translations/pl/index.js
@@ -1,5 +1,81 @@
+import automations from './Automations.aspx.json';
+import campaigns from './Campaigns.aspx.json';
+import notifications from './Notifications.json';
+import common from './Common.json';
+import landingPages from './LandingPages.aspx.json';
+import mms from './MmsCampaigns.aspx.json';
+import sms from './SMSCampaigns.aspx.json';
+import master from './Pulseem.master.json';
+import appBar from './AppBar.json';
+import report from './Reports.json';
+import mainReport from './MainReport.aspx.json';
+import dashboard from './Dashboard.json';
+import smsReport from './SMSReport.json';
+import emailStatus from './EmailStatus.json';
+import emoji from './Emoji.json';
+import payment from './payment.json';
+import siteTracking from './SiteTracking.json';
+import client from './Client.json';
+import recipient from './Recipient.json';
+import group from './group.json';
+import mmsreport from './MMSReport.json';
+import languages from './languages.json';
+import settings from './Settings.json'
+import whatsapp from './WhatsappCreator.json';
+import whatsappCampaign from './WhatsappCampaign.json';
+import whatsappManagement from './WhatsappManagement.json';
+import whatsappReport from './WhatsappReport.json';
+import whatsappChat from './WhatsappChat.json';
+import integrations from './Integrations.json'
+import WhatsappApiResponse from './WhatsappApiResponse.json'
+import SignUp from './SignUp.json';
+import FileUploads from './FileUploads.json';
+import SimplyClub from './WhiteLabel/SimplyClub.json';
+import affiliate from './Affiliate.json';
+import SubAccount from './SubAccount.json'
+import billing from './Billing.json';
+import TermsOfUse from './TermsOfUse.json'
+import WhatsappOnBoarding from './WhatsappOnBoarding.json'
 import RemoveMyData from './RemoveMyData.json'
 
 export const pl = {
-		RemoveMyData
+	automations,
+	campaigns,
+	notifications,
+	common,
+	landingPages,
+	mms,
+	sms,
+	appBar,
+	master,
+	report,
+	mainReport,
+	dashboard,
+	smsReport,
+	emailStatus,
+	emoji,
+	payment,
+	siteTracking,
+	client,
+	recipient,
+	group,
+	mmsreport,
+	languages,
+	settings,
+	integrations,
+	whatsapp,
+	whatsappCampaign,
+	whatsappManagement,
+	whatsappReport,
+	whatsappChat,
+	WhatsappApiResponse,
+	SignUp,
+	FileUploads,
+	SimplyClub,
+	affiliate,
+	SubAccount,
+	billing,
+	TermsOfUse,
+	WhatsappOnBoarding,
+	RemoveMyData
 };
