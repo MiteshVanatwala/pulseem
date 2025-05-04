b4ae175f7a746a2dd039b40f0e62ccb55ca36889
ido@pulseem.com <ido@pulseem.com>
2025-05-04 10:54:59 +0300

    PR-2287 - Added condition - Show lines if SmsBillingType === 0

--- src/screens/Sms/Editor/smsSummary.js
+++ src/screens/Sms/Editor/smsSummary.js
@@ -86,7 +86,7 @@ const SmsSummary = ({ classes,
                 </span>}
                 {summaryPayload?.SmsBillingType === 0 && <>
                   <span className={classes.bodySum}>
-                    {t("common.creditsPerMessage")}:
+                    {t("mmsreport.creditsPerMms")}:
                     <span className={classes.bodySum}> {summaryPayload?.CreditPerSms?.toLocaleString()}</span>
                   </span>
                   <span className={classes.bodySum}>
