2edfa492d98bc19ea447c36d3b3e3728ece7f7f7
Mitesh Vanatwala <vmitesh.282@gmail.com>
2026-04-28 08:36:56 +0530

    Remove 'BirthDate' from DATE_FIELDS in DisplayConditionsDialog to streamline date field validation

--- src/screens/HtmlCampaign/components/ContentDialogs/DisplayConditionsDialog.js
+++ src/screens/HtmlCampaign/components/ContentDialogs/DisplayConditionsDialog.js
@@ -235,7 +235,7 @@ const DisplayConditionsDialog = ({ onClose, save, args, classes }) => {
     [allFields]
   );
 
-  const DATE_FIELDS = ['BirthDate', 'ExtraDate1', 'ExtraDate2', 'ExtraDate3', 'ExtraDate4'];
+  const DATE_FIELDS = ['ExtraDate1', 'ExtraDate2', 'ExtraDate3', 'ExtraDate4'];
   const isDateField = (field) => DATE_FIELDS.includes(field);
 
   const escapeValue = (val) => (val || '').replace(/'/g, "\\'");
