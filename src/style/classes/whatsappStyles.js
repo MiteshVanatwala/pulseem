ff9e54e92edbe5e61bf6464c8e04a88903c6a9c6
Mitesh Vanatwala <mitesh@pulseem.com>
2026-08-20 12:32:03 +0530

    fix: add minWidth to chat style for better layout handling-issue of resoluation -nency

--- src/style/classes/whatsappStyles.js
+++ src/style/classes/whatsappStyles.js
@@ -1953,7 +1953,11 @@ export const getWhatsappStyle = (windowSize, isRTL, theme) => ({
 		// '@import url(./darktheme.css)': true,
 
 		//MainChat
-		'&.chat': { display: 'flex', position: 'relative' },
+		'&.chat': {
+			display: 'flex',
+			position: 'relative',			
+			minWidth: '0',			
+		},
 		'&.chat__body': {
 			minWidth: '300px',
 			flex: '40%',
